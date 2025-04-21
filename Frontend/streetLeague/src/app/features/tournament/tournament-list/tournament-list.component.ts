927454ecd68d533a4f687e8efe8d58227dcb9958
AnisBenOthman <anis12315@live.fr>
2025-04-21 18:42:10 +0100

    list of tournaments without call of api

--- Frontend/streetLeague/src/app/features/tournament/tournament-list/tournament-list.component.ts
+++ Frontend/streetLeague/src/app/features/tournament/tournament-list/tournament-list.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import { Status } from 'src/app/models/status';
+import { Tournament } from 'src/app/models/tournament';
+import { TournamentService } from 'src/app/services/tournament.service';
 
 @Component({
   selector: 'app-tournament-list',
   templateUrl: './tournament-list.component.html',
-  styleUrls: ['./tournament-list.component.css']
+  styleUrls: ['./tournament-list.component.css'],
 })
 export class TournamentListComponent {
+  tournaments: Tournament[] = [];
+  constructor(private tournamentService: TournamentService) {}
+  ngOnInit(): void {
+    this.getTournaments();
+  }
+  getTournaments(): void {
+    this.tournamentService.getTournaments().subscribe({
+      next: (data) => {
+        this.tournaments = data;
+        console.log('Tournaments fetched successfully:', this.tournaments);
+      },
+      error: (error) => {
+        console.error('Error fetching tournaments:', error);
+      },
+    });
+  }
+  getStatusLabel(status: Status): string {
+    switch (status) {
+      case Status.PENDING:
+        return 'En attente';
+      case Status.ONGOING:
+        return 'En cours';
+      case Status.FINISHED:
+        return 'Terminé';
+      case Status.PLANNED:
+        return 'Prévu';
 
+      default:
+        return 'Inconnu';
+    }
+  }
+
+  getStatusClass(status: Status): string {
+    switch (status) {
+      case Status.PENDING:
+        return 'status-pending';
+      case Status.ONGOING:
+        return 'status-active';
+      case Status.FINISHED:
+        return 'status-completed';
+      case Status.PLANNED:
+        return 'status-planned';
+      default:
+        return '';
+    }
+  }
 }
